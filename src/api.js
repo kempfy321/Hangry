228a78bee53ef7c54ccfc05a7cd1d559c19f1218
kempfy321 <shannon.r.kempf@gmail.com>
2018-10-24 21:47:36 -0400

    Updates media queries

--- src/api.js
+++ src/api.js
@@ -2,10 +2,11 @@
 
 const clientID = 'LISSRRKQURDJNW1VDULZPXAN1IT1SQW325YYZHPIKOSZKCEN';
 const clientSecret = 'VLT2MCQVLZFH2RPDOKTRFY0KJVI2ZRORNBULSUGGD2XREER1';
+
 const v = 20181018; //YYYYMMDD
 
 
-https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id={clientID}&client_secret={clientSecret}&v={v}
+// https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id={clientID}&client_secret={clientSecret}&v={v}
 
 
 // https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=LISSRRKQURDJNW1VDULZPXAN1IT1SQW325YYZHPIKOSZKCEN&client_secret=VLT2MCQVLZFH2RPDOKTRFY0KJVI2ZRORNBULSUGGD2XREER1&v=20181018
